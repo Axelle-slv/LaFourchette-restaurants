2f7a01e9408c52d9747b05c91861f9e7aeea23a4
Axelle-slv <axelle.silva@gmail.com>
2016-09-14 15:27:58 +0200

    Avancement de la page d'index et de la page de présentation du restaurant et ajout de visuels

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
  <link rel="stylesheet" href="fichiers/style.css">
   <!-- Fonts -->
  <link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700|Raleway:300,400,500" rel="stylesheet"> 
-
  </head>
 
  <body>
@@ -25,11 +24,11 @@
 </header>
 
 
- <php include ('header.php'); ?>
+ <?php include ('header.php'); ?>
 
 	<section class="container-fluid" id="philosophie">
 		<div class="container">
-			<h1>Lorem ipsum dolor sit amet, consectetur. </h1>
+			<h1>Lorem ipsum dolor sit amet, consectetur.</h1>
 			<hr />
 			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel posuere quam. Aliquam mollis ex. </h2>
 			
@@ -45,7 +44,7 @@
 
 	<section class="container-fluid hook-sentence" id="hook-index">
 		<div class="container">
-			<h1>A chaque fois, une expérience culinaire unique.</h1>
+			<h1>A chaque fois, une <span><strong>expérience culinaire</strong></span> unique.</h1>
 		</div>
 	</section>
 
@@ -53,14 +52,26 @@
 		<div class="container">
 			<h1>Les fondateurs de "La Fourchette".</h1>
 			<hr />
-			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel posuere quam. Aliquam mollis ex. </h2>
 			
-			<div class="col-xs-12 col-md-5 col-md-offset-1">
-				<p class="grey-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum. </p>
+			<div class="col-xs-12">
+				<div class="col-xs-12 col-md-4 col-md-offset-1">
+					<img src="fichiers/images/chef-damien-fondateur-la-fourchette.png" alt="Damien Armenté, co-fondateur des restaurants La Fourchette">
+				</div>
+				<div class="col-xs-12 col-md-6">
+					<h3>Lorem ipsum dolor sit amet, consectetur</h3>
+					<p class="grey-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum. </p>
+				</div>
 			</div>
 				
-			<div class="col-xs-12 col-md-5">
-				<p class="grey-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum. </p>
+			<div class="col-xs-12">
+				<div class="col-xs-12 col-md-6 col-md-offset-1">
+					<h3>Lorem ipsum dolor sit amet, consectetur</h3>
+					<p class="grey-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum. </p>
+				</div>
+
+				<div class="col-xs-12 col-md-4">
+					<img src="fichiers/images/chef-laurent-restaurant-la-fourchette.png" alt="Laurent Aubertin, co-fondateur et chef cuisinier des restaurants La Fourchette">
+				</div>
 			</div>
 		</div>
 	</section>
@@ -76,10 +87,11 @@
 		</div>
 	</section>
 
- <php include ('footer.php'); ?>
+ <?php include ('footer.php'); ?>
 
  </body>
 
- <!-- Bootstrap JavaScript -->
+<!-- Bootstrap JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
- </html>
+
+</html>
--- presentation-restaurant.php
+++ presentation-restaurant.php
@@ -5,61 +5,71 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Les restaurants gastronomiques La Fourchette travaillent en direct avec les producteurs locaux pour sélectionner les meilleurs produits et vous proposer des plats savoureux, originaux et colorés qui vous mettront l'eau à la bouche.">
 <title>Qui sommes-nous ? | La Fourchette | Restaurants gastronomiques</title>
+<!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="/fichiers/style.css">
+<!-- Our stylesheet -->
+<link rel="stylesheet" href="fichiers/style.css">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700|Raleway:300,400,500" rel="stylesheet"> 
 </head>
 
 <body>
 
 <php include ('header.php'); ?>
 
-	<section class="container-fluid" id="">
-		<div class="row">
-			<h1>
-			</h1>
+	<section class="container-fluid" id="who-hook">
+		<div class="container">
+			<h1>Notre leitmotiv, <span><strong>du local</strong></span> dans vos assiettes.</h1>
+			<img src="fichiers/images/frame-site-la-fourchette-white.png">
 		</div>
 	</section>
 
-	<section class="container-fluid" id="">
-		<div class="row">
-			<h1>
-			</h1>
-			<h2>
-			</h2>
-			<div class="content-container">
-				<div>
-				</div>
-				<div>
-				</div>
+	<section class="container-fluid" id="brand">
+		<div class="container">
+			<h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+			<hr />
+			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel posuere quam. Aliquam mollis ex. </h2>
+			
+			<div class="col-xs-12 col-md-5 col-md-offset-1">
+				<p class="white-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum.</p>
+			</div>
+			<div class="col-xs-12 col-md-5">
+				<p class="white-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum.</p>
 			</div>
 		</div>
 	</section>
 
-	<section class="container-fluid hook-sentence" id="">
-		<div class="row">
-			<h1>
-			</h1>
+	<section class="container-fluid hook-sentence" id="hook-presentation">
+		<div class="container">
+			<h1>Des recettes originales cuisinées avec de bons <span><strong>produits frais et bio.</strong></span></h1>
 		</div>
 	</section>
 
-	<section class="container-fluid" id="">
-		<div class="row">
-			<h1>
-			</h2>
-		</div>
-	</section>
-
-	<section class="container-fluid" id="">
-		<div class="row">
-			<h1>
-			</h1>
-			<h2>
-			</h2>
-			<div class="content-container">
-				<div>
+	<section class="container-fluid" id="products">
+		<div class="container">
+			<h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+			<hr />
+			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel posuere quam. Aliquam mollis ex. </h2>
+			
+			<div class="col-xs-10 col-xs-offset-1">
+				<div class="col-xs-4 meal">
+					<img src="fichiers/images/soupe-legumes-locaux-la-fourchette.jpg">
+					<span>Gaspacho betterave & tomate et son persil fraîchement cueilli<br /></span>
+					<small>Légumes cultivés avec passion et récoltés avec amour par des producteurs locaux.</small>
 				</div>
-				<div>
+				<div class="col-xs-4 meal">
+					<img src="fichiers/images/roti-agneau-la-fourchette.jpg">
+					<span>Agneau de 7 heures & sa sauce au gingembre caramélisé<br /></span>
+					<small>Légumes cultivés avec passion et récoltés avec amour par des producteurs locaux.</small>
 				</div>
+				<div class="col-xs-4 meal">
+					<img src="fichiers/images/dessert-maison-creation-originale-la-fourchette.jpg">
+					<span>Cheese Cake au fraises sauvage & chocolat blanc<br /></span>
+					<small>Légumes cultivés avec passion et récoltés avec amour par des producteurs locaux.</small>
+				</div>
+			</div>
+			<div class="col-xs-10 col-xs-offset-1">
+				<p class="grey-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies ligula ligula, eu porttitor mi maximus eu. Curabitur pharetra velit ac feugiat faucibus. In nec mi neque. Fusce ut libero nunc. Curabitur placerat at sapien ut sollicitudin. Integer ultricies consequat mi vitae gravida. Ut vitae sagittis massa. Aenean vel commodo nisi. Suspendisse pretium non lacus et sollicitudin. Donec volutpat at tellus id vulputate. Sed tristique tortor ut justo finibus blandit. Phasellus egestas lorem ac purus tristique condimentum. Aenean et dui aliquet, accumsan quam vel, rutrum libero. Sed eget malesuada nisi. Aliquam et odio cursus, finibus ligula id, euismod ex. Curabitur convallis massa eget enim mattis, in tincidunt ante rutrum.</p>
 			</div>
 		</div>
 	</section>
@@ -67,5 +77,8 @@
  <php include ('footer.php'); ?>
  
 </body>
+
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </html>
