03dec6c8d3e30bd3f8c98f5e11d098d3d8ee073f
RiadHacini <riad.hacini.pro@gmail.com>
2016-09-15 09:57:14 +0200

    Debut de la création du footer

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
-	<footer class="container-fluid">
-		<div class="row">
-			<div class="">
-			</div>
-			<div class="">
-			</div>
-			<div class="">
+	<footer class="container-fluid" id="footer-content">
+		<div class="container">
+			<div class="col-xs-10 col-xs-offset-1">
+				<div class="col-xs-4 "
 			</div>
 		</div>
 	</footer>
