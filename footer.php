edf5858c80f02b0c9fc790d08c8b3c4caf1c34f9
RiadHacini <riad.hacini.pro@gmail.com>
2016-09-15 11:49:34 +0200

    Footer terminé

--- footer.php
+++ footer.php
@@ -1,7 +1,33 @@
-	<footer class="container-fluid" id="footer-content">
+	
+
+	<footer class="container-fluid" id="footer-global">
 		<div class="container">
 			<div class="col-xs-10 col-xs-offset-1">
-				<div class="col-xs-4 "
+				
+				<div class="col-xs-3 footer-content">
+					<img id="logo-footer" src="fichiers/images/logolafourchettewhite.png">
+				</div>
+				
+				<div class="col-xs-6 footer-content">
+					<div class="col-xs-12">
+						
+						<div class="col-xs-6">
+						<p>Des questions sur la franchise ?</p>
+						<a href="devenir-franchise.php"> <button id="button-footer">Cliquez ici!</button></a>
+						</div>
+						
+						<div class="col-xs-6">
+						<p>Des informations sur nos restaurants?</p>
+						<a href="contact.php"> <button id="button-footer">Cliquez ici!</button></a>
+						</div>
+						
+					</div>
+				</div>
+
+				<div class="col-xs-3 footer-content">
+					<p>©Copyright <br />Tous droits réservés <br /> <a id="mentions-legales" href="mentions-legales.php" target="_blank">Mentions Légales </a></p>
+				</div>
+
 			</div>
 		</div>
 	</footer>
