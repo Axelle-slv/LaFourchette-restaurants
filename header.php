de93fd314756f0078f1b800b24430669f30d6ce0
Axelle-slv <axelle.silva@gmail.com>
2016-09-15 13:09:42 +0200

    Ajout de Jquery, ajustements dans index.php et début de création du header.php

--- header.php
+++ header.php
@@ -1,11 +1,26 @@
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default">
   <div class="container">
-  	<div class="col-xs-10 col-xs-offset-1">
-	<div class="navbar-header">
-      <a class="navbar-brand logo" href="#">
-        <img alt="La Fourchette | Restaurants gastronomique" src="fichiers/images/logolafourchette.png">
-      </a>
-    </div>
+    <div class="col-xs-10 col-xs-offset-1">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+      </div>
 
-  </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Qui sommes-nous ?</a></li>
+        <li><a href="#">Devenir franchisé</a></li>
+        <li><a href="#">Nous contacter</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+    </div><!-- /col-xs-10 -->
+  </div><!-- /.container -->
 </nav>
--- index.php
+++ index.php
@@ -23,8 +23,7 @@
 		<div class="container"></div>
 	</header>
 
-
- 	<?php include ('header.php'); ?>
+	<?php include ('header.php'); ?>
 
 	<section class="container-fluid" id="philosophie">
 		<div class="container">
@@ -89,9 +88,9 @@
 
  <?php include ('footer.php'); ?>
 
- </body>
-
-<!-- Bootstrap JavaScript -->
+<script type="text/javascript" src="fichiers/jquery-3.1.0.min.js"></script>
+ <!-- Bootstrap JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+</body>
 </html>
